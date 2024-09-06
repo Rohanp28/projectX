b195c2d4fcf2799d4f5d97c7e8b37bd19ef881f5
kushagra_mishra123 <kushagra.mishra@in.musafir.com>
2024-09-06 12:20:57 +0530

    new changes

--- app/index.jsx
+++ app/index.jsx
@@ -1,79 +1,116 @@
 import React from "react";
 import {
   Image,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
+import { useGlobalStyles } from "./GlobalStyles/GlobalStyles";
 
 export default function App() {
+  const [globalStyles, loaded] = useGlobalStyles();
+  if (!loaded) {
+    return <Text>loading ..</Text>;
+  }
   return (
-    <View style={styleSheet.container}>
-      {/* <View style={styleSheet.topHeaderWrapper}>
-        <View>X</View>
-        <View>Later</View>
-      </View> */}
-
-      <View style={styleSheet.greetingSection}>
-        <Text>Create an account</Text>
-      </View>
-      <View style={styleSheet.inputWrapper}>
-        <TextInput
-          placeholder="Enter your email"
-          style={styleSheet.inputContainer}
-        />
-        <TextInput
-          placeholder="Create password"
-          style={styleSheet.inputContainer}
-          secureTextEntry
-        />
-      </View>
-
-      <View style={styleSheet.createAccountContainer}>
-        <TouchableOpacity
-          style={styleSheet.createAccountContainer.createAccountBtn}
-        >
-          <Text style={{ fontWeight: "600", fontSize: 20 }}>
-            Create account
+    <SafeAreaView>
+      <View style={styleSheet.container}>
+        <View>
+          <Text
+            style={{
+              color: "white",
+              textAlign: "center",
+              fontWeight: 600,
+              ...globalStyles.text,
+              fontSize: 32,
+            }}
+          >
+            Project X
           </Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <View style={{ backgroundColor: "gray", height: 1, margin: 16 }}></View>
-      </View>
+        </View>
 
-      <View style={styleSheet.signInContainer}>
-        <TouchableOpacity style={styleSheet.signInContainer.signInContainerBtn}>
-          <View style={styleSheet.signInBtn}>
-            <Image
-              source={require("../assets/svg/google.png")}
-              style={styleSheet.socialIcons}
-            />
-            <Text>Continue with Google</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styleSheet.signInContainer.signInContainerBtn}>
-          <View style={styleSheet.signInBtn}>
-            <Image
-              source={require("../assets/svg/fb.png")}
-              style={styleSheet.socialIcons}
-            />
-            <Text>Continue with Facebook</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styleSheet.signInContainer.signInContainerBtn}>
-          <View style={styleSheet.signInBtn}>
-            <Image
-              source={require("../assets/svg/apple.png")}
-              style={styleSheet.socialIcons}
-            />
-            <Text>Continue with Apple</Text>
-          </View>
-        </TouchableOpacity>
+        <View style={styleSheet.greetingSection}>
+          <Text style={globalStyles.text}>Create an account</Text>
+        </View>
+        <View style={styleSheet.inputWrapper}>
+          <TextInput
+            placeholder="Enter your email"
+            style={{ ...globalStyles.text, ...styleSheet.inputContainer }}
+          />
+          <TextInput
+            placeholder="Create password"
+            style={{ ...globalStyles.text, ...styleSheet.inputContainer }}
+            secureTextEntry
+          />
+        </View>
+
+        <View style={styleSheet.createAccountContainer}>
+          <TouchableOpacity
+            style={styleSheet.createAccountContainer.createAccountBtn}
+          >
+            <Text style={globalStyles.text}>Start scrolling</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <View
+            style={{ backgroundColor: "gray", height: 1, margin: 16 }}
+          ></View>
+        </View>
+
+        <View style={styleSheet.signInContainer}>
+          <TouchableOpacity
+            style={styleSheet.signInContainer.signInContainerBtn}
+          >
+            <View style={styleSheet.signInBtn}>
+              <Image
+                source={require("../assets/svg/google.png")}
+                style={styleSheet.socialIcons}
+              />
+              <Text style={{ ...globalStyles.text, fontSize: 16 }}>
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styleSheet.signInContainer.signInContainerBtn}
+          >
+            <View style={styleSheet.signInBtn}>
+              <Image
+                source={require("../assets/svg/fb.png")}
+                style={styleSheet.socialIcons}
+              />
+              <Text style={{ ...globalStyles.text, fontSize: 16 }}>
+                Continue with Facebook
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styleSheet.signInContainer.signInContainerBtn}
+          >
+            <View style={styleSheet.signInBtn}>
+              <Image
+                source={require("../assets/svg/apple.png")}
+                style={styleSheet.socialIcons}
+              />
+              <Text
+                style={{
+                  ...globalStyles.text,
+                  fontSize: 16,
+                  fontWeight: "600",
+                }}
+              >
+                Continue with Apple
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -107,22 +144,29 @@ const styleSheet = StyleSheet.create({
   inputWrapper: {
     width: "100%",
     display: "flex",
+    gap: 10,
     flexDirection: "column",
   },
   inputContainer: {
     padding: 10,
     backgroundColor: "#ffff",
     width: "100%",
-    borderRadius: 4,
+    borderRadius: 10,
     color: "black",
     marginTop: 5,
+    fontSize: 14,
   },
   createAccountContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingLeft: 30,
+    paddingRight: 30,
     createAccountBtn: {
       elevation: 10,
       padding: 15,
       width: "100%",
-      borderRadius: 10,
+      borderRadius: 14,
       backgroundColor: "#F5F5F5",
       display: "flex",
       flexDirection: "row",
@@ -149,8 +193,8 @@ const styleSheet = StyleSheet.create({
   signInBtn: {
     display: "flex",
     flexDirection: "row",
-    gap: 5,
-    minWidth: 160,
+    gap: 6,
+    minWidth: 200,
     alignItems: "center",
   },
   socialIcons: {
