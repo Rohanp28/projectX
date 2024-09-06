546a5e3f950c23c36a4ff58a4aa191fdae56b630
rohan_patare <rohan.patare@in.musafir.com>
2024-09-06 12:39:34 +0530

    added welcome page

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Image,
   StyleSheet,
@@ -6,152 +6,154 @@ import {
   TextInput,
   TouchableOpacity,
   View,
-} from "react-native";
+} from 'react-native';
+import Welcome from './components/welcome';
 
 export default function App() {
   return (
-    <View style={styleSheet.container}>
-      {/* <View style={styleSheet.topHeaderWrapper}>
-        <View>X</View>
-        <View>Later</View>
-      </View> */}
+    <Welcome />
+    // <View style={styleSheet.container}>
+    //   {/* <View style={styleSheet.topHeaderWrapper}>
+    //     <View>X</View>
+    //     <View>Later</View>
+    //   </View> */}
 
-      <View style={styleSheet.greetingSection}>
-        <Text>Create an account</Text>
-      </View>
-      <View style={styleSheet.inputWrapper}>
-        <TextInput
-          placeholder="Enter your email"
-          style={styleSheet.inputContainer}
-        />
-        <TextInput
-          placeholder="Create password"
-          style={styleSheet.inputContainer}
-          secureTextEntry
-        />
-      </View>
+    //   <View style={styleSheet.greetingSection}>
+    //     <Text>Create an account</Text>
+    //   </View>
+    //   <View style={styleSheet.inputWrapper}>
+    //     <TextInput
+    //       placeholder="Enter your email"
+    //       style={styleSheet.inputContainer}
+    //     />
+    //     <TextInput
+    //       placeholder="Create password"
+    //       style={styleSheet.inputContainer}
+    //       secureTextEntry
+    //     />
+    //   </View>
 
-      <View style={styleSheet.createAccountContainer}>
-        <TouchableOpacity
-          style={styleSheet.createAccountContainer.createAccountBtn}
-        >
-          <Text style={{ fontWeight: "600", fontSize: 20 }}>
-            Create account
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <View style={{ backgroundColor: "gray", height: 1, margin: 16 }}></View>
-      </View>
+    //   <View style={styleSheet.createAccountContainer}>
+    //     <TouchableOpacity
+    //       style={styleSheet.createAccountContainer.createAccountBtn}
+    //     >
+    //       <Text style={{ fontWeight: "600", fontSize: 20 }}>
+    //         Create account
+    //       </Text>
+    //     </TouchableOpacity>
+    //   </View>
+    //   <View>
+    //     <View style={{ backgroundColor: "gray", height: 1, margin: 16 }}></View>
+    //   </View>
 
-      <View style={styleSheet.signInContainer}>
-        <TouchableOpacity style={styleSheet.signInContainer.signInContainerBtn}>
-          <View style={styleSheet.signInBtn}>
-            <Image
-              source={require("../assets/svg/google.png")}
-              style={styleSheet.socialIcons}
-            />
-            <Text>Continue with Google</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styleSheet.signInContainer.signInContainerBtn}>
-          <View style={styleSheet.signInBtn}>
-            <Image
-              source={require("../assets/svg/fb.png")}
-              style={styleSheet.socialIcons}
-            />
-            <Text>Continue with Facebook</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styleSheet.signInContainer.signInContainerBtn}>
-          <View style={styleSheet.signInBtn}>
-            <Image
-              source={require("../assets/svg/apple.png")}
-              style={styleSheet.socialIcons}
-            />
-            <Text>Continue with Apple</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    </View>
+    //   <View style={styleSheet.signInContainer}>
+    //     <TouchableOpacity style={styleSheet.signInContainer.signInContainerBtn}>
+    //       <View style={styleSheet.signInBtn}>
+    //         <Image
+    //           source={require("../assets/svg/google.png")}
+    //           style={styleSheet.socialIcons}
+    //         />
+    //         <Text>Continue with Google</Text>
+    //       </View>
+    //     </TouchableOpacity>
+    //     <TouchableOpacity style={styleSheet.signInContainer.signInContainerBtn}>
+    //       <View style={styleSheet.signInBtn}>
+    //         <Image
+    //           source={require("../assets/svg/fb.png")}
+    //           style={styleSheet.socialIcons}
+    //         />
+    //         <Text>Continue with Facebook</Text>
+    //       </View>
+    //     </TouchableOpacity>
+    //     <TouchableOpacity style={styleSheet.signInContainer.signInContainerBtn}>
+    //       <View style={styleSheet.signInBtn}>
+    //         <Image
+    //           source={require("../assets/svg/apple.png")}
+    //           style={styleSheet.socialIcons}
+    //         />
+    //         <Text>Continue with Apple</Text>
+    //       </View>
+    //     </TouchableOpacity>
+    //   </View>
+    // </View>
   );
 }
 
 const styleSheet = StyleSheet.create({
   container: {
-    height: "100%",
+    height: '100%',
     padding: 15,
-    display: "flex",
-    flexDirection: "column",
-    backgroundColor: "black",
-    color: "white",
-    justifyContent: "space-evenly",
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'black',
+    color: 'white',
+    justifyContent: 'space-evenly',
   },
   topHeaderWrapper: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   topHeader: {
-    display: "flex",
-    width: "100%",
-    justifyContent: "space-between",
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
   },
   greetingSection: {
-    display: "flex",
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "center",
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'center',
   },
   inputWrapper: {
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
   },
   inputContainer: {
     padding: 10,
-    backgroundColor: "#ffff",
-    width: "100%",
+    backgroundColor: '#ffff',
+    width: '100%',
     borderRadius: 4,
-    color: "black",
+    color: 'black',
     marginTop: 5,
   },
   createAccountContainer: {
     createAccountBtn: {
       elevation: 10,
       padding: 15,
-      width: "100%",
+      width: '100%',
       borderRadius: 10,
-      backgroundColor: "#F5F5F5",
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "center",
-      color: "black",
+      backgroundColor: '#F5F5F5',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      color: 'black',
     },
   },
   signInContainer: {
-    display: "flex",
+    display: 'flex',
     gap: 8,
 
     signInContainerBtn: {
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "center",
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
       gap: 5,
-      flexDirection: "row",
-      backgroundColor: "white",
+      flexDirection: 'row',
+      backgroundColor: 'white',
       padding: 10,
       borderRadius: 20,
       //   elevation: 10,
     },
   },
   signInBtn: {
-    display: "flex",
-    flexDirection: "row",
+    display: 'flex',
+    flexDirection: 'row',
     gap: 5,
     minWidth: 160,
-    alignItems: "center",
+    alignItems: 'center',
   },
   socialIcons: {
     width: 24,
