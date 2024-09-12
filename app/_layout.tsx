9815a6cde406839053afa64c39e16a79e64f3ea8
rohan_patare <rohan.patare@in.musafir.com>
2024-09-12 10:34:19 +0530

    hello

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,35 @@
-import React from "react";
-import { Stack } from "expo-router";
-import { KeyboardAvoidingView, SafeAreaView, StatusBar } from "react-native";
-import { StyleSheet } from "react-native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import React from 'react';
+import { Stack } from 'expo-router';
+import { KeyboardAvoidingView, SafeAreaView, StatusBar } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView>
       <StatusBar backgroundColor="white" />
-      <KeyboardAvoidingView behavior="height" style={{ display: "flex", flex: 1 }}>
+      <KeyboardAvoidingView
+        behavior="height"
+        style={{ display: 'flex', flex: 1 }}
+      >
         <SafeAreaView style={styleSheet.container}>
-          <Stack screenOptions={{
-            headerShown: false
-          }}>
-
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
             <Stack.Screen name="index" />
-          </Stack >
-        </SafeAreaView >
+          </Stack>
+        </SafeAreaView>
       </KeyboardAvoidingView>
     </GestureHandlerRootView>
-  )
+  );
 }
 
 const styleSheet = StyleSheet.create({
   container: {
-    display: "flex",
+    display: 'flex',
     marginTop: StatusBar.currentHeight,
-    flex: 1
-  }
-})
+    flex: 1,
+  },
+});
