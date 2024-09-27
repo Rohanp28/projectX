338327b8e462d9397ad2792555f3ff70d4c4c7fb
rohan_patare <rohan.patare@in.musafir.com>
2024-09-27 12:33:53 +0530

    test

--- Components/CustomButton/CustomButton.jsx
+++ Components/CustomButton/CustomButton.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { Pressable, StyleSheet, Text } from "react-native";
-import { useGlobalStyles } from "../GlobalStyles/GlobalStyles";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import React from 'react';
+import { Pressable, StyleSheet, Text } from 'react-native';
+import { useGlobalStyles } from '../GlobalStyles/GlobalStyles';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export const CustomButton = ({ buttonText, secondary, onPress }) => {
   const [globalStyles, loaded] = useGlobalStyles();
@@ -14,7 +14,7 @@ export const CustomButton = ({ buttonText, secondary, onPress }) => {
         style={{
           ...globalStyles.textBold,
           fontSize: 18,
-          color: secondary ? "white" : "black",
+          color: secondary ? 'white' : 'black',
         }}
       >
         {buttonText}
@@ -25,24 +25,24 @@ export const CustomButton = ({ buttonText, secondary, onPress }) => {
 
 const styleSheet = StyleSheet.create({
   button: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    width: "100%",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
     padding: 15,
     borderRadius: 10,
-    backgroundColor: "#FFFF33",
-    color: "black",
+    backgroundColor: '#FFFF33',
+    color: 'black',
   },
   secondaryButton: {
-    display: "flex",
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "center",
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
     elevation: 10,
     padding: 15,
     borderRadius: 10,
-    backgroundColor: "#69696980",
-    color: "white",
+    backgroundColor: '#69696980',
+    color: 'white',
   },
 });
